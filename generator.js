c610d297b158c4429093981d88dc8bf7a3446027
Richard Allen <richrad@gmail.com>
2012-08-18 12:45:14 -0500

    Frist post.

---
+++ generator.js
@@ -0,0 +1,22 @@
+var quote=new Array();
+
+quote[0] = "Bears discovered fire before man, but prefer their fish raw.";
+quote[1] = "Mitt Romney personally employs seven riding bears to help him navigate his underground fortress.";
+
+var attrib=new Array();
+attrib[0] = "Jane's Guide to the Modern Bear";
+attrib[1] = "Bears: No, You Prove to ME That They Aren't From Space";
+
+
+var randquote=Math.round(Math.random()*((quote.length)-1));
+var randattrib=Math.round(Math.random()*((attrib.length)-1));
+
+function showquote()
+	{
+	document.write(quote[randquote]);
+	}
+
+function showattrib()
+	{
+	document.write(attrib[randattrib]);
+	}
